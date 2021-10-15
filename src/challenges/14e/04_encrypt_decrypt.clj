d8e90f7e1f0440b79dd045776eb44b2906112371
MaterialFuture <konstantinev@protonmail.com>
2021-10-15 16:02:21 -0500

    fix encrypt/decrypt

--- src/challenges/14e/04_encrypt_decrypt.clj
+++ src/challenges/14e/04_encrypt_decrypt.clj
@@ -6,20 +6,20 @@
   (int (.charAt ch 0)))
 
 (defn str->enc->lazy-sec [string]
-  (for [x (for [x (into [] (map #(Character/getNumericValue %) string))
+  (for [x (for [x (into [] (map #(int %) string))
                 :let [y (* x 2)]]
             y)]
     (char x)))
 
 (defn encrypt [string]
-  (apply str (for [x (for [x (into [] (map #(Character/getNumericValue %) string))
+  (apply str (for [x (for [x (into [] (map #(int %) string))
                            :let [y (+ x 10)]]
                        y)]
                ;Instead of char there is perhaps another way to convert ascii vals to char
                (char x))))
 
 (defn decrypt [string]
-  (apply str (for [x (for [x (into [] (map #(Character/getNumericValue %) string))
+  (apply str (for [x (for [x (into [] (map #(int %) string))
                            :let [y (- x 10)]]
                        y)]
                (char x))))
