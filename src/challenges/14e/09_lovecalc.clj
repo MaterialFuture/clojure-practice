8c28c8366aa1c7a8eec34dc0e3213a4cfb63ffb0
konstantinev <konstantinev@protonmail.com>
2024-05-07 16:58:32 -0500

    finalize lovecalc feature

--- src/challenges/14e/09_lovecalc.clj
+++ src/challenges/14e/09_lovecalc.clj
@@ -5,13 +5,30 @@
 ;; Input = (sum (int name1) (int name2)) ...
 
 ;; Turn name string into a value based on ASCII
-(defn calcname [n] "Give each name a value based on ASCII values of each character."
-  (reduce + (into '() (map int (seq n)))))                      ;;Turn string into list of ints from ASCII values
+(defn calcname [n]
+  "Give each name a value based on ASCII values of each character."
+  (reduce + (into '() (map int (seq n)))))                      ;;Turn string into list of ints from ASCII values and sum it up
 
 ;; Output
-(defn calclove [n1 n2] "Compare both names and see the 'love' score."
-  (compare (calcname n1)                                    ;; WIP - compare values using some arbitrary metric
-           (calcname n2)))
+(defn calclove [n1 n2]
+  "Compare both names and see the 'love' score."
+  (let [y (calcname n2)
+        z (+ (calcname n1) (calcname n1))]
+      (str "Result: " (format "%.2f" (/ (* y 100.0) z)) "%")))      ;;The math sucks here but it's whatever, doesn't matter
+
+(defn calclove->debug [n1 n2]
+  "Compare both names and print results to hashmap for debugging."
+  (let [person1 {:name n1
+               :vals (map int (seq n1))
+               :total (calcname n1)}
+        person2 {:name n2
+                 :vals (map int (seq n2))
+                 :total (calcname n2)}]
+  (println person1 "\n" person2 "\n" (calclove n1 n2))))
+
+;; Calc - sum of x and y
+;; sum/100
+;; compare %
 
 ;; Extra - Create a hashmap of all the data for debugging
 ;; Extra 2 - Create a debugging log or print data in REPL
