39184c56eca0fa4b49821726cfb5168063cf3955
konstantinev <konstantinev@protonmail.com>
2024-05-07 20:55:35 -0500

    project euler started and 01 done

--- src/challenges/14e/07_euler.clj
+++ src/challenges/14e/07_euler.clj
@@ -1,7 +1,10 @@
+;; Project Euler
+;; Source: https://projecteuler.net/
+
 (ns challenges.14e.07-euler)
 
-;; project euler -> https://projecteuler.net/
-
-(defn euler01 []
+(defn euler01 [n]
   "Find the sum of all the multiples of 3 or 5 below 1000."
-  )
+    (reduce + (remove nil? (for [x (range n)]
+                                (cond (zero? (mod x 5)) x
+                                      (zero? (mod x 3)) x)))))
