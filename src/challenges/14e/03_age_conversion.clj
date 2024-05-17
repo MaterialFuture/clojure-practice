3b8d665351ac7265b2d92af77c8e50f917ed97a3
konstantinev <konstantinev@protonmail.com>
2024-05-17 01:54:30 -0500

    Updating challenge #3 and RPS (#6)

--- src/challenges/14e/03_age_conversion.clj
+++ src/challenges/14e/03_age_conversion.clj
@@ -6,20 +6,34 @@
 (defn years->seconds [years]
   (* years 31536000))
 
-;; Bonus - Other time conversion methods
+;; Bonus - Other time conversion methods (assuming non-leap year)
+;; years
 (defn years->months [years]
   (* years 12))
+(defn years->weeks [years]
+  (* years 52))
 (defn years->days [years]
   (* years 365))
-(defn months->seconds [months]
-  (* months 2628000))
-(defn days->seconds [days]
-  (* days 86400))
+(defn years->hours [years]
+  (* years 8760))
+(defn years->minutes [years]
+  (* years 525600))
+;; months omited due to the amount of variation
+;;weeks
+(defn weeks->days [weeks]
+  (* weeks 7))
+(defn weeks->minutes [weeks]
+  (* weeks 168))
+(defn weeks->seconds [weeks]
+  (* weeks 86400))
+
+;; Other bonus -> Converting birthdate to get the most accurate assessment of time
 
 ;; Going upward
 (defn seconds->minutes [seconds]
   (/ seconds 60))
-
+(defn minutes->hours [seconds]
+  (/ seconds 60))
 
 (defn run-tests [& args]
   (println "Running tests...")
--- src/challenges/14e/06_rps.clj
+++ src/challenges/14e/06_rps.clj
@@ -6,17 +6,36 @@
   (let [hands ["Rock" "Paper" "Scissors"]]
     (nth (shuffle hands) 1)))
 
+(defn print-hand [a b c]
+  (println a "\n" b "\n" c))                                ;;format the print statement
+
 (defn rock-paper-scissors [hand]
   "It so far only works for rock"
-  (let [possible-hands ["Rock" "Paper" "Scissors"]]
+  (let [player (clojure.string/capitalize hand)]
+   (let [possible-hands ["Rock" "Paper" "Scissors"]]
     (let [cpu-hand (nth (shuffle possible-hands) 1)]
       (cond
-        (and (= hand "Rock")
-             (= cpu-hand "Rock")) "Tie"
-        (and (= hand "Rock")
-             (= cpu-hand "Paper")) "You Lose..."
-        (and (= hand "Rock")
-             (= cpu-hand "Scissors")) "You Win!"))))
+        (and (= player "Rock")
+             (= cpu-hand "Rock")) (print-hand player cpu-hand "Tie")
+        (and (= player "Rock")
+             (= cpu-hand "Paper")) (print-hand player cpu-hand "You Lose")
+        (and (= player "Rock")
+             (= cpu-hand "Scissors")) (print-hand player cpu-hand "You Win!")
+        ;;Paper
+        (and (= player "Paper")
+             (= cpu-hand "Rock")) (print-hand player cpu-hand "You Win!")
+        (and (= player "Paper")
+             (= cpu-hand "Paper")) (print-hand player cpu-hand "Tie")
+        (and (= player "Paper")
+             (= cpu-hand "Scissors")) (print-hand player cpu-hand "You lose...")
+        ;;Scissors
+        (and (= player "Scissors")
+             (= cpu-hand "Rock")) (print-hand player cpu-hand "You lose")
+        (and (= player "Scissors")
+             (= cpu-hand "Paper")) (print-hand player cpu-hand "You Win!")
+        (and (= player "Scissors")
+             (= cpu-hand "Scissors")) (print-hand player cpu-hand "Tie")
+        :else "I don't recognize that... Please try again!")))))
 
 ;Notes: by putting create-hand in a lambda it eliminates the need to the function
 ;Nested lets are useful when working with immutable data
