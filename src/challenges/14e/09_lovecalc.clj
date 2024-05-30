8f29ee453fa6917907d77485b13aec3dd391398c
MaterialFuture <konstantinev@protonmail.com>
2024-05-30 16:11:02 -0500

    update 5.30 - testing calva

--- src/challenges/14e/09_lovecalc.clj
+++ src/challenges/14e/09_lovecalc.clj
@@ -14,17 +14,17 @@
   "Compare both names and see the 'love' score."
   (let [y (calcname n2)
         z (+ (calcname n1) (calcname n1))]
-      (str "Result: " (format "%.2f" (/ (* y 100.0) z)) "%")))      ;;The math sucks here but it's whatever, doesn't matter
+    (str "Result: " (format "%.2f" (/ (* y 100.0) z)) "%")))      ;;The math sucks here but it's whatever, doesn't matter
 
 (defn calclove->debug [n1 n2]
   "Compare both names and print results to hashmap for debugging."
   (let [person1 {:name n1
-               :vals (map int (seq n1))
-               :total (calcname n1)}
+                 :vals (map int (seq n1))
+                 :total (calcname n1)}
         person2 {:name n2
                  :vals (map int (seq n2))
                  :total (calcname n2)}]
-  (println person1 "\n" person2 "\n" (calclove n1 n2))))
+    (println person1 "\n" person2 "\n" (calclove n1 n2))))
 
 ;; Calc - sum of x and y
 ;; sum/100
